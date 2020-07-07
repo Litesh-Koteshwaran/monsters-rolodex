60e4e5ee0ef21df0ace035525eab002e54786e1d
Litesh K <58351442+Litesh-Koteshwaran@users.noreply.github.com>
2020-07-07 16:20:56 +0530

    Updates

---
+++ precache-manifest.5a0d32cf3002a3fcdecdde68f58422f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0fb49fa436183d8189f0f94b88f94d8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1c08bac428e739cb8138",
+    "url": "/monsters-rolodex/static/css/main.0377d9e5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c08bac428e739cb8138",
+    "url": "/monsters-rolodex/static/js/main.81a5745d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
